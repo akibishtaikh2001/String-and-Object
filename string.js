d7d18eb41f093754323a2835371576fe5fc2527c
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-02 02:42:55 +0600

    String VS Array.

---
+++ string.js
@@ -0,0 +1,30 @@
+const Country = 'Bangladesh';
+const division = 'New khal e';
+const distict = `B-Baria`;
+// const thana = new String('Demra');
+
+// console.log( thana);
+
+const numbers = [54, 98, 78, 21, 65];
+console.log(numbers);
+
+// console.log(numbers.length);
+// console.log(numbers[1]);
+numbers[1] = 11;
+console.log(numbers);
+
+// string is immutable --> Not Changeable
+// array is mutable --> you can change the array elements
+
+const capital = 'Dhaka';
+// console.log(capital.length);
+// console.log(capital[3]);
+
+console.log(capital);
+capital[0] = 'F';
+console.log(capital);
+
+
+
+
+
