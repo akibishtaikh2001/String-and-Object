7edeecfac96a6c89d217bdb393f4e5d3d496e7ca
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-02 21:02:14 +0600

    Object Keys

---
+++ keys.js
@@ -0,0 +1,10 @@
+const computer = {
+    brand: 'lenevo',
+    price: 35000,
+    procesor: 'intel',
+    sdd: '512gb',
+    monitor: 'hp'
+}
+
+const keys = Object.keys(computer);
+console.log(keys);
