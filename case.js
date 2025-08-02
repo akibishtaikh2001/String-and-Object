6b7be6446802bd51fe6759f64bdd408956bb56f9
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-02 13:25:12 +0600

    String case

---
+++ case.js
@@ -0,0 +1,32 @@
+const school = 'RAJ UK uttara Model School';
+
+console.log(school);
+
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+
+
+const subject = 'Chemistry';
+const book = 'chemIsTry';
+
+if (subject.toLowerCase() === book.toLowerCase()) {
+    console.log(' I am reading book aibar porikkhai fatai frlmu');
+
+}
+
+else {
+    console.log('hudai hudai pristha ultai ar vat khwar jone opekha kori');
+
+}
+
+const drink = ' water';
+const liquid = 'water ';
+
+if (drink.trim() === liquid.trim()) {
+    console.log('paani er upor nam life');
+
+}
+else {
+    console.log('shumudre pani ache khaite pari na');
+
+}
